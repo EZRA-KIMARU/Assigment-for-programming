9073c64d08dff1ef927820db41480082a4aa4798
EZRA-KIMARU <102142739+EZRA-KIMARU@users.noreply.github.com>
2022-04-12 16:10:34 +0300

    Add files via upload

---
+++ program to check eligibity of voting.c.c
@@ -0,0 +1,19 @@
+//program to check voting eligibility 
+#include<stdio.h>
+int main()
+{
+  int age,nationality,kenyan;
+  printf("Enter your age:");
+  scanf("%d",&age);
+  printf("Enter your nationality");
+  scanf("%d",&nationality);
+  if(age>=18 & kenyan)
+  {
+    printf("You are eligible to vote");
+  }
+  else
+  {
+    printf("You are not eligible to vote");
+  }
+    return 0;
+}
