724e52dec98c2c993c99d8a357fc6873d305867e
EZRA-KIMARU <102142739+EZRA-KIMARU@users.noreply.github.com>
2022-04-12 17:02:10 +0300

    Add files via upload

---
+++ program to display numbers in reverse.c
@@ -0,0 +1,19 @@
+//program to display numbers in reverse
+#include<stdio.h>
+int main()
+{
+  int n = 50;
+  while (n>0)
+    {
+    printf("%d",n);
+    n = n - 1;
+    }
+    printf("Loop body finished");
+
+
+
+
+
+
+  return 0;
+  }
